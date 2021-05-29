57fe5aefe1fa2b9577d5ac8243ce9778b9080416
rohan <rohanibajpai@gmail.com>
2021-05-29 18:30:39 -0400

    missing homepage, otherwise functional

--- MarshallSciFair/settings.py
+++ MarshallSciFair/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@*$v-h)+$3fkcu_xfo)q17ui2*0(@m&t$io8pvj)o9x_&r%x*#'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '192.168.1.219']
+ALLOWED_HOSTS = []
 
 
 # Application definition
